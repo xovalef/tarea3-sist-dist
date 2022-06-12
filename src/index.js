138396ba28f1669f2998fa4fd430fd04b4a9edb1
rolitoxdd <rolo.alv97@gmail.com>
2022-06-12 01:57:39 -0400

    ALDKFJLAKDFSJLSKJDAF

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-const cassandra=require('cassandra-driver')
-client=new cassandra.Client({
-    contactPoints:["cassandra-node1"],
-    localDataCenter:'cassandra-cluster',
-    credentials:{username:'cassandra',
-    password:'password123'
-}})
-//client.execute("select uuid() from system.local").then(console.log)
-//client.execute("select uuid() from system.local").then(data=>x=data)
-//x.rows[0]['system.uuid()'].toString()
+const cassandra = require("cassandra-driver");
+const fastify = require("fastify")({
+  logger: true,
+});
 
-const fastify = require('fastify')({
-    logger:true
-})
+client = new cassandra.Client({
+  contactPoints: ["cassandra-node1"],
+  localDataCenter: "datacenter1",
+  credentials: { username: "cassandra", password: "password123" },
+});
 
-fastify.get('/', async (request, reply) => {
-    reply.type('application/json').code(200)
-    const query = "SELECT * FROM ks2.recetas;"
-    const data = await client.execute(query)
-    return { hello: 'world', data}
-})
-  
-fastify.listen({ port: 3000, host:'0.0.0.0' }, (err, address) => {
-    if (err) throw err
-        console.log(`Server is now listening on ${address}`)
-})
+fastify.get("/", async (request, reply) => {
+  reply.type("application/json").code(200);
+  const query = "SELECT * FROM ks2.recetas;";
+  const data = await client.execute(query);
+  return { hello: "world", data: data.rows };
+});
+
+fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+  if (err) throw err;
+  console.log(`Server is now listening on ${address}`);
+});
